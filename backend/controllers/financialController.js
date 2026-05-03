5b0e3f4ec709f6b49ef7266187ad5f015794e9f0
VISHALSAI BJ <74887520+GREATORMAN@users.noreply.github.com>
2026-05-03 21:09:51 +0530

    Delete backend directory

--- backend/controllers/financialController.js
+++
@@ -1,9 +0,0 @@
-import { calculateEMI as calcEMI, calculateEarlyPaymentBenefit as calcBenefit } from '../utils/financial.js'
-
-export const calculateEMI = (principal, rate, tenure) => {
-  return calcEMI(principal, rate, tenure)
-}
-
-export const calculateEarlyPaymentBenefit = (principal, rate, tenure, extraPayment) => {
-  return calcBenefit(principal, rate, tenure, extraPayment)
-}
